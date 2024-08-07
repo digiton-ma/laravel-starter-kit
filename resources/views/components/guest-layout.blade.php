1b1f257d910c7d70cc070dd2caec64693a71d3e5
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2024-08-07 01:55:16 +0100

    created a guest layout with navigation, footer, stacks and seo options.

---
+++ resources/views/components/guest-layout.blade.php
@@ -0,0 +1,76 @@
+@php
+    $generalSettings = app(\App\Settings\GeneralSettings::class);
+@endphp
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    @stack('head::start')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="{{ $meta_description ?? $generalSettings->meta_description ?? '' }}">
+
+    <title>
+        {{ (isset($title) ? $title . ' | ' : '') . ($generalSettings->site_name ?? config('app.name', 'Laravel')) }}
+    </title>
+
+    <link rel="canonical" href="{{ url()->current() }}" />
+    <link rel="icon" href="{{ \Illuminate\Support\Facades\Storage::url($generalSettings->site_favicon) ?? asset('assets/logos/favicon.ico') }}" type="image/x-icon"/>
+
+    {{-- Open Graph / Facebook --}}
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:title" content="{{ $title ?? config('app.name', 'Laravel') }}">
+    <meta property="og:description" content="{{ $meta_description ?? $generalSettings->meta_description ?? '' }}">
+    <meta property="og:image" content="{{ $og_image ?? \Illuminate\Support\Facades\Storage::url($generalSettings->og_image) ?? asset('images/og-image.jpg') }}">
+
+    {{-- Twitter --}}
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="{{ url()->current() }}">
+    <meta property="twitter:title" content="{{ $title ?? config('app.name', 'Laravel') }}">
+    <meta property="twitter:description" content="{{ $meta_description ?? $generalSettings->meta_description ?? '' }}">
+    <meta property="twitter:image" content="{{ $og_image ?? \Illuminate\Support\Facades\Storage::url($generalSettings->og_image) ?? asset('images/og-image.jpg') }}">
+
+    <!-- Custom Google font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+    @stack('head::before-scripts')
+
+    @livewireStyles
+    @vite('resources/css/app.css')
+    @stack('styles')
+
+    @vite('resources/js/app.js')
+    @stack('head::scripts')
+
+    {{-- JSON-LD --}}
+    <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "WebSite",
+            "name": "{{ config('app.name', 'Laravel') }}",
+            "url": "{{ config('app.url') }}"
+        }
+    </script>
+
+    @stack('head::end')
+</head>
+<body class="antialiased" dir="{{(app()->isLocale('ar') ? 'rtl' : 'ltr')}}">
+    @stack('body::start')
+    <x-navigation />
+
+    @stack('body::before-main')
+    <main>
+        {{ $slot }}
+    </main>
+    @stack('body::after-main')
+
+    <x-footer />
+    @livewireScriptConfig
+    @stack("body::scripts")
+
+    @stack('body::end')
+</body>
+</html>
