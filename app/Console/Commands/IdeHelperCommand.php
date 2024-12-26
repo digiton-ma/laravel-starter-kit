9b0ea692ed9f0074c73a5d7c4698b3930824e900
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2024-12-26 14:52:30 +0100

    updated composer json and created a new ide-helper generation command.

---
+++ app/Console/Commands/IdeHelperCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class IdeHelperCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ide-helper:generate-safe';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate IDE Helper files only in non-production environments';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if (app()->environment('production')) {
+            $this->info('Skipping IDE Helper generation in production.');
+            return 0;
+        }
+
+        $this->call('ide-helper:generate');
+        $this->call('ide-helper:meta');
+
+        return 0;
+    }
+}
