2f3a28f69f91910b08be9031e84123fb405d4e14
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2025-02-13 13:17:00 +0100

    cleaned up the home page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,8 @@
     <x-slot name="title">
         Home
     </x-slot>
-    test home page
+
+    <section class="h-screen w-screen flex justify-center items-center bg-gradient-to-tl from-gray-300 to-[30%] to-gray-50">
+        <h1 class="text-3xl text-gray-400">Home Page</h1>
+    </section>
 </x-guest-layout>
