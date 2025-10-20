18c9b81e432ac59b19f3bc7b593a8de8ef8c28b4
Mehdi Mekouar <contact@mehdimekouar.com>
2025-10-20 14:28:27 +0100

    Add configuration file for Laravel Glide Helper with default settings

---
+++ config/glide-helper.php
@@ -0,0 +1,11 @@
+<?php
+
+// config for Mehdismekouar/LaravelGlideHelper
+return [
+    'defaults' => [
+        'q' => 90,
+        'fm' => 'webp',
+        'fit' => 'max'
+    ],
+    'output_dir' => 'manipulated',
+];
