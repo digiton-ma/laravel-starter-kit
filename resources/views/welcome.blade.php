513b0af7b69ace85d90bdc7c9440205059f554b9
Mehdi Mekouar <contact@mehdimekouar.com>
2025-10-20 15:53:05 +0100

    Update .gitignore to include composer.lock and package-lock.json; remove unused package from composer.json; enhance welcome page layout with spacing adjustments

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
         Home
     </x-slot>
 
-    <section class="h-screen w-screen flex justify-center items-center bg-gradient-to-tl from-gray-300 to-[30%] to-gray-50">
+    <section class="h-screen w-screen flex flex-col gap-10 justify-center items-center bg-gradient-to-tl from-gray-300 to-[30%] to-gray-50">
         <h1 class="text-3xl text-gray-400">Home Page</h1>
     </section>
 </x-guest-layout>
